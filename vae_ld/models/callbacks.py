6464805bf750ee7bc41816da2b51f762a3e86acf
L.Bonheme <lb732@kent.ac.uk>
2021-10-01 14:44:56 +0100

    Fix image generator callback

--- vae_ld/models/callbacks.py
+++ vae_ld/models/callbacks.py
@@ -1,43 +1,18 @@
-import os
-import tempfile
-from pathlib import Path
-import numpy as np
 import tensorflow as tf
 from tensorflow import sigmoid
-from tensorflow.python.distribute import multi_worker_util
 
 
-class FileSaverCallback(tf.keras.callbacks.Callback):
-    """ Generic class implementing get_file_path to retrieve create a file name using log content
-    """
-    def _get_file_path(self, logs, epoch):
-        """ Returns the file path for checkpoint. Similarly to tf.keras.callbacks.ModelCheckpoint
-        """
-        # noinspection PyProtectedMember
-        if not self.model._in_multi_worker_mode() or multi_worker_util.should_save_checkpoint():
-            try:
-                return self.filepath.format(epoch=epoch, **logs)
-            except KeyError as e:
-                raise KeyError("Failed to format this callback filepath: \"{}\". Reason: {}".format(self.filepath, e))
-        else:
-            self._temp_file_dir = tempfile.mkdtemp()
-            extension = os.path.splitext(self.filepath)[1]
-            return os.path.join(self._temp_file_dir, "temp" + extension)
-
-
-class ImageGeneratorCallback(FileSaverCallback):
-    def __init__(self, *, filepath, nb_samples, data_path, latent_shape):
+class ImageGeneratorCallback(tf.keras.callbacks.Callback):
+    def __init__(self, *, filepath, nb_samples, data, latent_shape):
         """ Initialisation of image generator callback
-        :param filepath: the path used for saving the activations
-        :param data_path: the path of the dataset used for retrieving the activation values
-        The dataset is expected to be a npz file containing a data key where the data is stored
+        :param nb_samples: The number of examples to generate
+        :param data: the dataset used for sampling examples
         """
         super(ImageGeneratorCallback, self).__init__()
         self.nb_samples = nb_samples
         self.latent_shape = latent_shape
+        self.data = data[:nb_samples]
         self.filepath = filepath
-        data_path = str(Path(data_path).expanduser())
-        self.data = np.load(data_path)["data"][:nb_samples]
 
     def save_images(self, generated_images, file_path):
         generated_images = sigmoid(generated_images)
@@ -48,18 +23,13 @@ class ImageGeneratorCallback(FileSaverCallback):
             img.save("{}.{}.png".format(file_path, i))
 
     def generate_images(self, samples, file_path):
-        generated_images = self.model.decoder(samples, training=False)
-        if type(generated_images) == list:
-            generated_images = generated_images[-1]
+        generated_images = self.model.decoder(samples, training=False)[-1]
         self.save_images(generated_images, file_path)
 
     def on_epoch_end(self, epoch, logs=None):
-        logs = logs if logs else {}
-        file_path = self._get_file_path(logs, epoch)
         if epoch == 0:
-            self.save_images(self.data, "{}.real_data".format(file_path))
+            self.save_images(self.data, "{}/data_example".format(self.filepath, epoch))
         random_latent_vectors = tf.random.normal(shape=(self.nb_samples, self.latent_shape))
-        self.generate_images(random_latent_vectors, "{}.from_random_latents".format(file_path))
-        out = self.model.encoder(self.data, training=False)
-        _, _, z = out[-3:]
-        self.generate_images(z, "{}.from_real_data".format(file_path))
+        self.generate_images(random_latent_vectors, "{}/epoch_{}_from_random_latents".format(self.filepath, epoch))
+        z = self.model.encoder(self.data, training=False)[-1]
+        self.generate_images(z, "{}/epoch_{}_from_real_data".format(self.filepath, epoch))
