4f5f8b8d3c46185646fd9df6da726ccf7215210a
Lisa Bonheme <lb732@kent.ac.uk>
2021-10-15 07:50:40 +0100

    Implement TwoNN IDE

--- vae_ld/learning_dynamics/intrinsic_dimension_estimators.py
+++ vae_ld/learning_dynamics/intrinsic_dimension_estimators.py
@@ -1,6 +1,8 @@
 from scipy.spatial.distance import squareform, pdist
 import numpy as np
 
+from vae_ld.learning_dynamics import logger
+
 
 class TwoNN:
     """ Implementation of the ID estimator TwoNN from [1]
@@ -35,22 +37,35 @@ class TwoNN:
             Alessio Ansuini, Alessandro Laio, Jakob H. Macke, and Davide Zoccolan, 2019
         """
         # 1. Compute the pairwise distances for each point in the dataset
-        x_dist = np.sort(squareform(pdist(X)), axis=1)
+        logger.info("Computing the pairwise distance between each point of the dataset")
+        x_dist = np.sort(squareform(pdist(X)), axis=1, kind="heapsort")
 
         # 2. Get two shortest distances
+        logger.info("Getting the two shortest distances")
         r1 = x_dist[:, 1]
         r2 = x_dist[:, 2]
 
+        logger.info("Removing zero values and degeneracies")
+        zeros = np.where(r1 == 0)[0]
+        degeneracies = np.where(r1 == r2)[0]
+        good = np.setdiff1d(np.arange(x_dist.shape[0]), np.array(zeros))
+        good = np.setdiff1d(good, np.array(degeneracies))
+        r1 = r1[good]
+        r2 = r2[good]
+
         # 3. For each point i compute mu_i
-        mu = np.sort(r2/r1)
+        logger.info("Computing mu_i for each point i")
+        mu = np.sort(r2/r1, kind="heapsort")
 
         # 4. Compute the empirical cumulate Femp(mu)
+        logger.info("Computing the empirical cumulate")
         n = r1.shape[0]
         Femp = np.arange(0, n, dtype=np.float64) / n
 
         # 5. Fit the points of the plane given by coordinates {(log(mu_i), -log(1 - Femp(mu_i)))|i=1, …, n} with a
         # straight line passing through the origin, using the analytical solution of the linear regression.
         # Note that we discard 10% of the points by default, as recommended in the TwoNN paper
+        logger.info("Fitting the {}% first points with a linear regression".format(self._to_keep * 100))
         n_to_keep = int(n * self._to_keep)
         x = np.log(mu)[:n_to_keep]
         y = -np.log(1 - Femp)[:n_to_keep]
