b80bdbf9ebaa99dcdaf2f2aa176e0e2b4bc7b4d7
Lisa Bonheme <lb732@kent.ac.uk>
2021-10-18 17:21:56 +0100

    Save images only every 100 epochs

--- vae_ld/models/callbacks.py
+++ vae_ld/models/callbacks.py
@@ -3,7 +3,7 @@ from tensorflow import sigmoid
 
 
 class ImageGeneratorCallback(tf.keras.callbacks.Callback):
-    def __init__(self, *, filepath, nb_samples, data, latent_shape):
+    def __init__(self, *, filepath, nb_samples, data, latent_shape, save_freq):
         """ Initialisation of image generator callback
         :param nb_samples: The number of examples to generate
         :param data: the dataset used for sampling examples
@@ -13,6 +13,7 @@ class ImageGeneratorCallback(tf.keras.callbacks.Callback):
         self.latent_shape = latent_shape
         self.data = data[:nb_samples]
         self.filepath = filepath
+        self.save_freq = save_freq
 
     def save_images(self, generated_images, file_path):
         generated_images = sigmoid(generated_images)
@@ -29,7 +30,9 @@ class ImageGeneratorCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         if epoch == 0:
             self.save_images(self.data, "{}/data_example".format(self.filepath, epoch))
-        random_latent_vectors = tf.random.normal(shape=(self.nb_samples, self.latent_shape))
-        self.generate_images(random_latent_vectors, "{}/epoch_{}_from_random_latents".format(self.filepath, epoch))
-        z = self.model.encoder(self.data, training=False)[-1]
-        self.generate_images(z, "{}/epoch_{}_from_real_data".format(self.filepath, epoch))
+        if epoch % self.save_freq == 0:
+            random_latent_vectors = tf.random.normal(shape=(self.nb_samples, self.latent_shape))
+            self.generate_images(random_latent_vectors, "{}/epoch_{}_from_random_latents".format(self.filepath, epoch))
+            z = self.model.encoder(self.data, training=False)[-1]
+            self.generate_images(z, "{}/epoch_{}_from_real_data".format(self.filepath, epoch))
+
