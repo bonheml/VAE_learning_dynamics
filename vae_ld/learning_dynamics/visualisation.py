50a3f87ffff437fad7ffce1bd6a099e0b980fbdf
L.Bonheme <lb732@kent.ac.uk>
2021-11-29 08:17:54 +0000

    Improve CKA visualisation

--- vae_ld/learning_dynamics/visualisation.py
+++ vae_ld/learning_dynamics/visualisation.py
@@ -5,11 +5,13 @@ from pathlib import Path
 from vae_ld.learning_dynamics.utils import save_figure
 
 
-def cka_heatmap(input_dir):
+def cka_heatmap(cfg):
     sns.set_style("whitegrid", {'axes.grid': False, 'legend.labelspacing': 1.2})
-    files = glob("{}/*.tsv".format(input_dir))
-    dfs = [(Path(f).stem, pd.from_csv(f, sep="\t")) for f in files]
+    files = glob("{}/*.tsv".format(cfg.input_dir))
+    dfs = [(Path(f).stem, pd.read_csv(f, sep="\t", header=0, index_col=0)) for f in files]
     for f, df in dfs:
         save_path = "{}.pdf".format(f)
-        sns.heatmap(df)
+        ax = sns.heatmap(df)
+        ax.set(xlabel="{}, {}={}, seed={}".format(cfg.m1_name, cfg.p1_name, cfg.p1_value, cfg.m1_seed),
+               ylabel="{}, {}={}, seed={}".format(cfg.m2_name, cfg.p2_name, cfg.p2_value, cfg.m2_seed))
         save_figure(save_path)
