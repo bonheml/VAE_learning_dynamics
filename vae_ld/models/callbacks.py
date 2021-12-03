f5f3b6ba470cb364cebac2b925ccf28216d75913
L.Bonheme <lb732@kent.ac.uk>
2021-12-03 10:08:30 +0000

    Improve image callback

--- vae_ld/models/callbacks.py
+++ vae_ld/models/callbacks.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 from tensorflow import sigmoid
+import numpy as np
+import matplotlib.pyplot as plt
+
+from vae_ld.learning_dynamics.utils import save_figure
 
 
 class ImageGeneratorCallback(tf.keras.callbacks.Callback):
@@ -15,24 +19,27 @@ class ImageGeneratorCallback(tf.keras.callbacks.Callback):
         self.filepath = filepath
         self.save_freq = save_freq
 
-    def save_images(self, generated_images, file_path):
-        generated_images = sigmoid(generated_images)
-        generated_images *= 255.
-        generated_images = generated_images.numpy()
-        for i in range(self.nb_samples):
-            img = tf.keras.preprocessing.image.array_to_img(generated_images[i])
-            img.save("{}.{}.png".format(file_path, i))
+    def plot_and_save(self, imgs, fname):
+        n = int(np.sqrt(imgs[0].shape[0]))
+        n_sources = len(imgs)
+        fig = plt.figure(figsize=(n, n * n_sources))
 
-    def generate_images(self, samples, file_path):
-        generated_images = self.model.decoder(samples, training=False)[-1]
-        self.save_images(generated_images, file_path)
+        for i in range(0, imgs[0].shape[0]):
+            for j in range(n_sources):
+                plt.subplot(n, n * n_sources, (n_sources * i) + j + 1)
+                plt.imshow(imgs[j][i, :, :, 0], cmap='gray')
+                plt.axis('off')
+
+        save_figure(fname)
 
     def on_epoch_end(self, epoch, logs=None):
-        if epoch == 0:
-            self.save_images(self.data, "{}/data_example".format(self.filepath, epoch))
         if epoch % self.save_freq == 0:
             random_latent_vectors = tf.random.normal(shape=(self.nb_samples, self.latent_shape))
-            self.generate_images(random_latent_vectors, "{}/epoch_{}_from_random_latents".format(self.filepath, epoch))
+            generated_images = sigmoid(self.model.decoder(random_latent_vectors, training=False)[-1]) * 255.
+            self.plot_and_save([generated_images], "{}/epoch_{}_from_random_latents.pdf".format(self.filepath, epoch))
+
             z = self.model.encoder(self.data, training=False)[-1]
-            self.generate_images(z, "{}/epoch_{}_from_real_data".format(self.filepath, epoch))
+            generated_images = sigmoid(self.model.decoder(z, training=False)[-1]) * 255.
+            self.plot_and_save([self.data, generated_images],
+                               "{}/epoch_{}_from_real_data.pdf".format(self.filepath, epoch))
 
