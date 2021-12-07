569c22916cdfcea9276f0cd3ab34b2fa42449136
Lisa Bonheme <lb732@kent.ac.uk>
2021-12-07 20:13:59 +0000

    Make MNIST compatible with DataSampler

--- vae_ld/data/mnist.py
+++ vae_ld/data/mnist.py
@@ -1,18 +1,31 @@
 import numpy as np
 import tensorflow as tf
+from vae_ld.data import util
+from vae_ld.data.dataset import Data
 
-class Mnist:
-    def __init__(self, *, observation_shape, **kwargs):
-        self._data = None
-        self._observation_shape = tuple(observation_shape)
 
-    @property
-    def data(self):
-        return self._data
+class Mnist(Data):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._data, self._features, self._lookup_table = self.load_data()
 
     def load_data(self):
-        (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
-        mnist_digits = np.concatenate([x_train, x_test], axis=0)
-        mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
-        self._data = mnist_digits
-        return mnist_digits
+        (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.mnist.load_data()
+        mnist_digits = np.concatenate([X_train, X_test], axis=0)
+        mnist_features = np.concatenate([Y_train, Y_test], axis=0)
+        mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255.
+        lookup_table = []
+        for i in range(10):
+            lookup_table.append(np.where(mnist_features == i)[0])
+        return mnist_digits, mnist_features, lookup_table
+
+    def sample_factors(self, batch_size, seed):
+        """Sample a batch of factors Y."""
+        return seed.choice(10, batch_size)
+
+    def sample_observations_from_factors(self, factors, seed):
+        """Sample a batch of observations X given a batch of factors Y."""
+        indices = []
+        for i in factors:
+            indices.append(self._lookup_table[i][seed.choice(len(self._lookup_table[i]), 1)][0])
+        return self._data[indices]
