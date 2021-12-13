bfcc7a8c9f35f59a844c12163ffe7913151d4bbd
Lisa Bonheme <lb732@kent.ac.uk>
2021-12-13 11:14:58 +0000

    Move cka parsing function to utils

--- vae_ld/learning_dynamics/utils.py
+++ vae_ld/learning_dynamics/utils.py
@@ -1,4 +1,10 @@
+from glob import glob
+from pathlib import Path
+
 import matplotlib.pyplot as plt
+import numpy as np
+
+from vae_ld.data.util import natural_sort
 
 
 def save_figure(out_fname, dpi=300):
@@ -7,3 +13,37 @@ def save_figure(out_fname, dpi=300):
     plt.clf()
     plt.cla()
     plt.close()
+
+
+def get_model_filename(model, model_name, param_value):
+    m_seed = model.partition("checkpoint")[0].strip("/").split("/")[-1]
+    m_short = Path(model).name
+    return "{}_{}_seed_{}_{}".format(model_name, param_value, m_seed, m_short)
+
+
+def get_file_list(model_path, keep_n, selection_type="even"):
+    m_files = glob(model_path)
+    m_files.sort(key=natural_sort)
+    if keep_n > 0 and selection_type == "even":
+        to_keep = np.linspace(0, len(m_files) - 1, num=keep_n, dtype=np.int32)
+        m_files = [m_files[i] for i in to_keep]
+    elif keep_n > 0 and selection_type == "first":
+        return m_files[:keep_n]
+    return m_files
+
+
+def get_model_epoch(model, dataset_name):
+    mstem = Path(model).stem
+    i = mstem.find("epoch_")
+    # TODO remove hard-coded epochs
+    last_epoch = 26
+    if dataset_name == "cars3d":
+        last_epoch = 1090
+    elif dataset_name == "smallnorb":
+        last_epoch = 410
+    if i == -1:
+        epoch = last_epoch
+    else:
+    # TODO End remove here
+        epoch = int(mstem[i:].split("_")[1])
+    return epoch
