f0a8345bafff6be93115bfcdf54b08120ba4d68f
Lisa Bonheme <lb732@kent.ac.uk>
2022-01-19 12:31:00 +0000

    Update image generator callback output

--- vae_ld/models/callbacks.py
+++ vae_ld/models/callbacks.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
 from tensorflow import sigmoid
-import numpy as np
-import matplotlib.pyplot as plt
-
-from vae_ld.visualisation.utils import save_figure
+from vae_ld.visualisation.images import plot_and_save
 
 
 class ImageGeneratorCallback(tf.keras.callbacks.Callback):
@@ -19,28 +16,13 @@ class ImageGeneratorCallback(tf.keras.callbacks.Callback):
         self.filepath = filepath
         self.save_freq = save_freq
 
-    def plot_and_save(self, imgs, fname):
-        n = int(np.sqrt(imgs[0].shape[0]))
-        n_sources = len(imgs)
-        fig = plt.figure(figsize=(n * n_sources, n))
-
-        for i in range(0, imgs[0].shape[0]):
-            for j in range(n_sources):
-                plt.subplot(n * n_sources, n, (n_sources * i) + j + 1)
-                plt.imshow(imgs[j][i, :, :, 0], cmap='gray')
-                plt.axis('off')
-
-        fig.subplots_adjust(wspace=0, hspace=0)
-        save_figure(fname)
-
     def on_epoch_end(self, epoch, logs=None):
         if epoch % self.save_freq == 0:
             random_latent_vectors = tf.random.normal(shape=(self.nb_samples, self.latent_shape))
             generated_images = sigmoid(self.model.decoder(random_latent_vectors, training=False)[-1]) * 255.
-            self.plot_and_save([generated_images], "{}/epoch_{}_from_random_latents.pdf".format(self.filepath, epoch))
+            plot_and_save(generated_images, "{}/epoch_{}_from_random_latents.pdf".format(self.filepath, epoch))
 
             z = self.model.encoder(self.data, training=False)[-1]
             generated_images = sigmoid(self.model.decoder(z, training=False)[-1]) * 255.
-            self.plot_and_save([self.data, generated_images],
-                               "{}/epoch_{}_from_real_data.pdf".format(self.filepath, epoch))
+            plot_and_save(generated_images, "{}/epoch_{}_from_real_data.pdf".format(self.filepath, epoch), self.data)
 
