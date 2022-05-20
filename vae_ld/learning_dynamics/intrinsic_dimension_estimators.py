eec5726891d12a29df243770b2258d34ccc156f3
Lisa Bonheme <lb732@kent.ac.uk>
2022-05-20 10:24:04 +0100

    Improve logging of IDE

--- vae_ld/learning_dynamics/intrinsic_dimension_estimators.py
+++ vae_ld/learning_dynamics/intrinsic_dimension_estimators.py
@@ -1,8 +1,4 @@
-import random
-
-from scipy.spatial.distance import squareform, pdist
 import numpy as np
-from sklearn import linear_model
 import gibbs
 from sklearn.neighbors import NearestNeighbors
 from vae_ld.learning_dynamics import logger
@@ -63,18 +59,21 @@ class TwoNN:
         .. [2] Ansuini, A., Laio, A., Macke, J. H., & Zoccolan, D. (2019). Intrinsic dimension of data representations
                in deep neural networks. Advances in Neural Information Processing Systems, 32.
         """
+        logger.info("Computing TwoNN with {}% of the data kept.".format(self._to_keep * 100))
         self._knn.fit(X)
         # 1. Compute the pairwise distances for each point in the dataset
-        logger.info("Computing the pairwise distance between each point of the dataset")
+        logger.debug("Computing the pairwise distance between each point of the dataset")
+        # An equivalent computation of this is
         # x_dist = np.sort(squareform(pdist(X)), axis=1, kind="heapsort")
         x_dist = self._knn.kneighbors(X)[0]
 
         # 2. Get two shortest distances
-        logger.info("Getting the two shortest distances")
+        logger.debug("Getting the two shortest distances")
         r1 = x_dist[:, 1]
         r2 = x_dist[:, 2]
 
         # This step was added in Ansuini et al. implementation
+        # However, it does not seem necessary if the data contains no duplicates
         # logger.info("Removing zero values and degeneracies")
         # zeros = np.where(r1 == 0)[0]
         # degeneracies = np.where(r1 == r2)[0]
@@ -85,18 +84,18 @@ class TwoNN:
         # r2 = r2[good]
 
         # 3. For each point i compute mu_i
-        logger.info("Computing mu_i for each point i")
+        logger.debug("Computing mu_i for each point i")
         mu = np.sort(r2/r1, kind="heapsort")
 
         # 4. Compute the empirical cumulate Femp(mu)
-        logger.info("Computing the empirical cumulate")
+        logger.debug("Computing the empirical cumulate")
         n = r1.shape[0]
         Femp = np.arange(0, n, dtype=np.float64) / n
 
         # 5. Fit the points of the plane given by coordinates {(log(mu_i), -log(1 - Femp(mu_i)))|i=1, …, n} with a
         # straight line passing through the origin, using the analytical solution of the linear regression.
         # Note that we discard 10% of the points by default, as recommended in the TwoNN paper
-        logger.info("Fitting the {}% first points with a linear regression".format(self._to_keep * 100))
+        logger.debug("Fitting the {}% first points with a linear regression".format(self._to_keep * 100))
         n_to_keep = int(n * self._to_keep)
         x = np.log(mu)[:n_to_keep]
         y = -np.log(1 - Femp)[:n_to_keep]
