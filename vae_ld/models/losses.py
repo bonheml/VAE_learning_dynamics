da8ad928855295827975c308adab2a8e11767494
L.Bonheme <lb732@kent.ac.uk>
2022-06-10 12:38:44 +0100

    Add SSIM reconstrunction loss

--- vae_ld/models/losses.py
+++ vae_ld/models/losses.py
@@ -30,3 +30,44 @@ class BernoulliLoss:
         loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y_true)
         loss_flatten = tf.reshape(loss, shape=[-1, flattened_dim])
         return tf.reduce_sum(loss_flatten, axis=1)
+
+
+class SSIMLoss:
+    """ Use the SSIM similarity metric [1] to compute a distance normalised in [0,1].
+    We assume that pixel values are between 0 and 1. The distance computed is
+
+    .. math::
+       SSIM_loss(y_true, y_pred) = 1 - (SSIM(y_true, y_pred) + 1) / 2
+
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        The tensor of n_examples true images
+    y_pred : tf.Tensor
+        The tensor of n_examples predicted images
+
+    Returns
+    -------
+    tf.Tensor
+        The array of n_examples normalised ssim losses
+
+    Examples
+    --------
+    >>> loss = SSIMLoss()
+    >>> np.random.seed(0)
+    >>> y_true = tf.constant(np.random.random((1,64,64,3)))
+    >>> y_pred = tf.constant(np.random.random((1,64,64,3)))
+    >>> loss(y_true, y_pred)
+     <tf.Tensor: shape=(1,), dtype=float32, numpy=array([0.5001584], dtype=float32)>
+    >>> loss(y_pred, y_true)
+     <tf.Tensor: shape=(1,), dtype=float32, numpy=array([0.5001584], dtype=float32)>
+    >>> loss(y_true, y_true)
+    <tf.Tensor: shape=(1,), dtype=float32, numpy=array([0.], dtype=float32)>
+    References
+    ----------
+    .. [1]  Wang, Z., Bovik, A. C., Sheikh, H. R., & Simoncelli, E. P. (2004). Image quality assessment:
+            from error visibility to structural similarity. IEEE transactions on image processing.
+    """
+    def __call__(self, y_true, y_pred):
+        norm_ssim = 1 - (tf.image.ssim(y_pred, y_true, 1.0) + 1) / 2
+        return norm_ssim
