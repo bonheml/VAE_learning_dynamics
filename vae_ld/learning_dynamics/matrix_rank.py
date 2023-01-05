97572a6450a03b4c7f48e0aa68799ad1032aa2d3
L.Bonheme <lb732@kent.ac.uk>
2023-01-05 16:14:23 +0000

    Add simple rank for fondue

--- vae_ld/learning_dynamics/matrix_rank.py
+++ vae_ld/learning_dynamics/matrix_rank.py
@@ -3,6 +3,8 @@ from vae_ld.learning_dynamics import logger
 
 
 class EffectiveRank:
+    def __init__(self, erank=True):
+        self.erank = erank
 
     def fit_transform(self, X):
         """Computes the effective rank of a representation using implementation from
@@ -24,9 +26,10 @@ class EffectiveRank:
         sv = np.linalg.svd(cov, compute_uv=False)
         logger.debug("Singular values : {}".format(sv))
         non_zero_sv = sv[sv != 0]
-        logger.debug("Rank: {}".format(non_zero_sv.shape[0]))
+        rank = non_zero_sv.shape[0]
+        logger.info("Rank: {}".format(rank))
         non_zero_sv_norm = non_zero_sv / non_zero_sv.sum()
         sv_entropy = -(non_zero_sv_norm * np.log(non_zero_sv_norm)).sum()
-        er = np.exp(sv_entropy)
-        logger.info("Effective rank: {}".format(er))
-        return er
+        erank = np.exp(sv_entropy)
+        logger.info("Effective rank: {}".format(erank))
+        return erank if self.erank else rank
