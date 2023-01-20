27632673443a35dfb26510bee6a5e908f2f8731c
L.Bonheme <lb732@kent.ac.uk>
2023-01-20 16:38:02 +0000

    Update DT and round fondue with CT

--- vae_ld/learning_dynamics/trace.py
+++ vae_ld/learning_dynamics/trace.py
@@ -3,6 +3,14 @@ from vae_ld.learning_dynamics import logger
 
 
 class CovarianceTrace:
+    def __init__(self, precision=None):
+        """
+        Parameters
+        ----------
+        precision : int or None
+            The precision to which the covariance matrix is rounded
+        """
+        self._precision = precision
 
     def fit_transform(self, X):
         """Computes the trace of the covariance of a representation
@@ -18,6 +26,8 @@ class CovarianceTrace:
             The trace of cov[X]
         """
         cov = np.cov(X, rowvar=False)
+        if self._precision:
+            cov = cov.round(decimals=self._precision)
         logger.debug("Cov[X] = {}".format(cov))
         tr = np.trace(cov)
         logger.debug("Tr(Cov[X]) = {}".format(tr))
